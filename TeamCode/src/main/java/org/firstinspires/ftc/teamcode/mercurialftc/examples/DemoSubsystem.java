ec2234b942dc598c2e44dd64ecbfcf598a45f89d
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-10-28 23:46:58 +1100

    updated to mercurial v0.0.2, changed demoSubsystem to handle its control differently

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mercurialftc/examples/DemoSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mercurialftc/examples/DemoSubsystem.java
@@ -38,6 +38,8 @@ public class DemoSubsystem extends Subsystem {
 		motor2 = new CachingDcMotorEX(opModeEX.hardwareMap.get(DcMotorEx.class, "motor2"));
 		
 		resetEncodersInternal();
+		
+		setDefaultCommand(runTo(position));
 	}
 	
 	@Override
@@ -47,57 +49,6 @@ public class DemoSubsystem extends Subsystem {
 	
 	@Override
 	public void defaultCommandExecute() {
-		PIDupdate();
-	}
-	
-	/**
-	 * sets up the PID loop handled in the default command to run to a new target position
-	 * <p>note that this command does not require the subsystem, this is because it should not be interrupting as it does not need to manipulate the hardware devices directly, and there is no harm in running it, it also ends instantly</p>
-	 *
-	 * @param targetPosition new target position
-	 * @return a command with the specified behaviour
-	 */
-	public Command runTo(int targetPosition) {
-		return new LambdaCommand()
-				.setInterruptible(true)
-				.setInit(() -> prepPID(targetPosition));
-	}
-	
-	public Command runTo(@NonNull Positions position) {
-		return runTo(position.getPosition());
-	}
-	
-	public Command manualControl(DoubleSupplier controller) {
-		return new LambdaCommand()
-				.setRequirements(this)
-				.setRunStates(OpModeEX.OpModeEXRunStates.LOOP)
-				.setInterruptible(true)
-				.setExecute(() -> {
-					motor1.setPower(controller.getAsDouble());
-					motor2.setPower(controller.getAsDouble());
-				})
-				.setFinish(() -> false)
-				.setEnd((interrupted) -> {
-					this.targetPosition = position;
-				});
-	}
-	
-	public Command resetEncoders() {
-		return new LambdaCommand()
-				.setInterruptible(false)
-				.setRunStates(OpModeEX.OpModeEXRunStates.INIT_LOOP, OpModeEX.OpModeEXRunStates.LOOP)
-				.setInit(this::resetEncodersInternal)
-				.setFinish(() -> true);
-	}
-	
-	private void resetEncodersInternal() {
-		motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-		motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-	}
-	
-	public void PIDupdate() {
 		double kp = 1;
 		double ki = 1;
 		double kd = 1;
@@ -115,6 +66,52 @@ public class DemoSubsystem extends Subsystem {
 		
 		previousTime = time;
 		previousError = error;
+	}
+	
+	/**
+	 * sets up the PID loop handled in the default command to run to a new target position
+	 * <p>note that this command does not require the subsystem, this is because it should not be interrupting as it does not need to manipulate the hardware devices directly, and there is no harm in running it, it also ends instantly</p>
+	 *
+	 * @param targetPosition new target position
+	 * @return a command with the specified behaviour
+	 */
+	public Command runTo(int targetPosition) {
+		return new LambdaCommand()
+				.setInterruptible(true)
+				.setInit(() -> prepPID(targetPosition))
+				.setExecute(this::defaultCommandExecute) // uses the default command execute
+				.setFinish(() -> false);
+	}
+	
+	public Command runTo(@NonNull Positions position) {
+		return runTo(position.getPosition());
+	}
+	
+	public Command manualControl(DoubleSupplier controller) {
+		return new LambdaCommand()
+				.setRequirements(this)
+				.setRunStates(OpModeEX.OpModeEXRunStates.LOOP)
+				.setInterruptible(true)
+				.setExecute(() -> {
+					motor1.setPower(controller.getAsDouble());
+					motor2.setPower(controller.getAsDouble());
+				})
+				.setFinish(() -> false);
+	}
+	
+	public Command resetEncoders() {
+		return new LambdaCommand()
+				.setInterruptible(false)
+				.setRunStates(OpModeEX.OpModeEXRunStates.INIT_LOOP, OpModeEX.OpModeEXRunStates.LOOP)
+				.setInit(this::resetEncodersInternal)
+				.setFinish(() -> true);
+	}
+	
+	private void resetEncodersInternal() {
+		motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 	}
 	
 	public void prepPID(int targetPosition) {
